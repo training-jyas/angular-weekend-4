2ff04b1f0abfdcc0200ef8d7fe251549c1b1a2b5
Jyasveer Gotta <jyasveer@gmail.com>
2018-05-19 13:50:09 +0530

    directives done

--- e-cart/src/app/dashboard/dashboard.module.ts
+++ e-cart/src/app/dashboard/dashboard.module.ts
@@ -3,12 +3,16 @@ import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { ProductComponent } from './product/product.component';
+import { DropdownDirective } from './product/dropdown.directive';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [DashboardComponent, ProductDetailComponent, ProductComponent],
+  declarations: [
+    DashboardComponent, ProductDetailComponent, ProductComponent,
+    DropdownDirective
+  ],
   exports: [DashboardComponent]
 })
 export class DashboardModule { }
